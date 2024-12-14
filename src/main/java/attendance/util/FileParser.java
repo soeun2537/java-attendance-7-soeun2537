92dcc27d7e0e13769c651298a455b1c84adbdac4
soeun2537 <soeun2537@gmail.com>
2024-12-14 17:51:59 +0900

    refactor: 하드코딩 추출

--- src/main/java/attendance/util/FileParser.java
+++ src/main/java/attendance/util/FileParser.java
@@ -2,6 +2,7 @@ package attendance.util;
 
 import static attendance.constant.InputConstant.*;
 
+import attendance.constant.InputConstant;
 import attendance.constant.PathConstant;
 import attendance.model.Attendance;
 import java.util.ArrayList;
@@ -17,8 +18,9 @@ public class FileParser {
         List<String> readLines = FileReader.readFile(PathConstant.ATTENDANCE_FILE_PATH.getPath());
         for (String readLine : readLines) {
             List<String> attendance = Parser.separateBySeparator(readLine, FILE_SEPARATOR.getContent());
-            attendances.add(Attendance.from(attendance.get(0), Parser.parseDateTime(attendance.get(1), " ")));
-
+            attendances.add(Attendance.from(
+                    attendance.get(0),
+                    Parser.parseDateTime(attendance.get(1), SPACE.getContent())));
         }
         return attendances;
     }
