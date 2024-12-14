8d5d26bbf719490894d9ebb52c9f97794633d734
soeun2537 <soeun2537@gmail.com>
2024-12-14 17:39:51 +0900

    feat: FileParser 구현

---
+++ src/main/java/attendance/util/FileParser.java
@@ -0,0 +1,25 @@
+package attendance.util;
+
+import static attendance.constant.InputConstant.*;
+
+import attendance.constant.PathConstant;
+import attendance.model.Attendance;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileParser {
+
+    private FileParser() {
+    }
+
+    public static List<Attendance> readAttendanceFile() {
+        List<Attendance> attendances = new ArrayList<>();
+        List<String> readLines = FileReader.readFile(PathConstant.ATTENDANCE_FILE_PATH.getPath());
+        for (String readLine : readLines) {
+            List<String> attendance = Parser.separateBySeparator(readLine, FILE_SEPARATOR.getContent());
+            attendances.add(Attendance.from(attendance.get(0), Parser.parseDateTime(attendance.get(1), " ")));
+
+        }
+        return attendances;
+    }
+}
