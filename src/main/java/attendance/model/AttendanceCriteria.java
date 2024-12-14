66a18d7736324d5802748e1b9233c6be12c8689d
soeun2537 <soeun2537@gmail.com>
2024-12-14 17:50:36 +0900

    refactor: 함수 라인 추출 및 변수 정리

--- src/main/java/attendance/model/AttendanceCriteria.java
+++ src/main/java/attendance/model/AttendanceCriteria.java
@@ -5,34 +5,44 @@ import java.time.format.TextStyle;
 import java.util.Locale;
 
 public enum AttendanceCriteria {
-    월(13, 18),
-    화(10, 18),
-    수(10, 18),
-    목(10, 18),
-    금(10, 18),
+    월(13),
+    화(10),
+    수(10),
+    목(10),
+    금(10),
     ;
 
     private final int startTime;
-    private final int endTime;
 
-    AttendanceCriteria(int startTime, int endTime) {
+    AttendanceCriteria(int startTime) {
         this.startTime = startTime;
-        this.endTime = endTime;
     }
 
     public static AttendanceCheck checkAttendance(LocalDateTime localDateTime) {
         for (AttendanceCriteria value : values()) {
-            if (value.name().equals(localDateTime.getDayOfWeek().getDisplayName(TextStyle.NARROW, Locale.KOREA))) {
-                if (value.startTime == localDateTime.getHour()) {
-                    if (localDateTime.getMinute() <= 5) {
-                        return AttendanceCheck.출석;
-                    }
-                    if (localDateTime.getMinute() <= 30) {
-                        return AttendanceCheck.지각;
-                    }
-                    return AttendanceCheck.결석;
-                }
+            AttendanceCheck 출석 = getAttendanceCheck(localDateTime, value);
+            if (출석 != null) {
+                return 출석;
             }
+        }
+        return AttendanceCheck.결석;
+    }
+
+    private static AttendanceCheck getAttendanceCheck(LocalDateTime localDateTime, AttendanceCriteria value) {
+        if (value.name().equals(localDateTime.getDayOfWeek().getDisplayName(TextStyle.NARROW, Locale.KOREA))) {
+            if (value.startTime == localDateTime.getHour()) {
+                return getAttendanceCheck(localDateTime);
+            }
+        }
+        return null;
+    }
+
+    private static AttendanceCheck getAttendanceCheck(LocalDateTime localDateTime) {
+        if (localDateTime.getMinute() <= 5) {
+            return AttendanceCheck.출석;
+        }
+        if (localDateTime.getMinute() <= 30) {
+            return AttendanceCheck.지각;
         }
         return AttendanceCheck.결석;
     }
