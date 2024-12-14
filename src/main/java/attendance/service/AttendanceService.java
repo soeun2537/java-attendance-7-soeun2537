4d2acb7da60bfebe4c529224737551228a07453b
soeun2537 <soeun2537@gmail.com>
2024-12-14 17:39:26 +0900

    feat: AttendanceService 구현

---
+++ src/main/java/attendance/service/AttendanceService.java
@@ -0,0 +1,63 @@
+package attendance.service;
+
+import static attendance.constant.InputConstant.*;
+
+import attendance.model.Attendance;
+import attendance.model.AttendanceRecord;
+import attendance.model.Subject;
+import attendance.repository.AttendanceRepository;
+import attendance.util.FileParser;
+import attendance.util.Parser;
+import camp.nextstep.edu.missionutils.DateTimes;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+
+public class AttendanceService {
+
+    private final AttendanceRepository attendanceRepository;
+
+    public AttendanceService(AttendanceRepository attendanceRepository) {
+        this.attendanceRepository = attendanceRepository;
+    }
+
+    public void presetting() {
+        attendanceRepository.initialize();
+        List<Attendance> attendances = FileParser.readAttendanceFile();
+        for (Attendance attendance : attendances) {
+            attendanceRepository.add(attendance);
+        }
+    }
+
+    public LocalDateTime addAttendance(String nickname, String attendanceTime) {
+        String dateTime = Parser.attach(DateTimes.now().toString(), attendanceTime, SPACE.getContent());
+        LocalDateTime attendance = Parser.parseDateTime(dateTime, "T");
+        attendanceRepository.add(Attendance.from(nickname, attendance));
+        return attendance;
+    }
+
+    public List<AttendanceRecord> modifyAttendance(String nickname, int day, String time) {
+        return attendanceRepository.modify(nickname, day, time);
+    }
+
+    public List<AttendanceRecord> getAttendance(String nickname) {
+        return attendanceRepository.getAttendances(nickname);
+    }
+
+    public List<Integer> getTotal(String nickname) {
+        return attendanceRepository.getTotal(nickname);
+    }
+
+    public String getSubject(String nickname) {
+        return attendanceRepository.getSubject(nickname);
+    }
+
+    public List<Subject> findSubjects() {
+        return attendanceRepository.findSubjects();
+    }
+
+    public Set<String> getNicknames() {
+        return attendanceRepository.findAllNickname();
+    }
+}
