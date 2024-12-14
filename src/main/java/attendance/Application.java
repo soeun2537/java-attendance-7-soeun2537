bfb474df1b3dfe674d9f712bb969b5e0cb2ea944
soeun2537 <soeun2537@gmail.com>
2024-12-14 17:38:04 +0900

    feat: Application 설정

--- src/main/java/attendance/Application.java
+++ src/main/java/attendance/Application.java
@@ -1,7 +1,15 @@
 package attendance;
 
+import attendance.controller.AttendanceController;
+import attendance.repository.AttendanceRepository;
+import attendance.service.AttendanceService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AttendanceRepository attendanceRepository = new AttendanceRepository();
+        AttendanceService attendanceService = new AttendanceService(attendanceRepository);
+        attendanceService.presetting();
+        AttendanceController attendanceController = new AttendanceController(attendanceService);
+        attendanceController.run();
     }
 }
